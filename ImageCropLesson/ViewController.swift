2098c8a776077773b701a57da96c87742d63be97
Iram Fattah <iramfattah94@gmail.com>
2020-01-09 10:33:20 -0500

    add touches began and touches moved

--- ImageCropLesson/ViewController.swift
+++ ImageCropLesson/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cropButton: UIButton!
     
+    //MARK: Properties
+    private let strokeColor: UIColor = UIColor.purple
+    private let lineWidth: CGFloat = 2.0
+    private var shapeLayer = CAShapeLayer()
+    private var path = UIBezierPath()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +40,33 @@ class ViewController: UIViewController {
     }
     
     
+    private func addPathToShapeLayer() {
+        shapeLayer.path = self.path.cgPath
+        shapeLayer.strokeColor = self.strokeColor.cgColor
+        shapeLayer.lineWidth = self.lineWidth
+        imageView.layer.addSublayer(shapeLayer)
+        
+    }
+    
+    
 
+}
+
+
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchPoint = touch.location(in: self.imageView)
+            path.move(to: touchPoint)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchPoint = touch.location(in: imageView)
+            path.addLine(to: touchPoint)
+            addPathToShapeLayer()
+        }
+    }
 }
 
